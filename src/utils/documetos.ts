ec39c442b60f1f485950618d67b0ed14504b7436
Eliezer Nunes <eliezernun@gmail.com>
2024-02-13 19:22:07 -0300

    criar serviço incluir colaborador

--- src/utils/documetos.ts
+++ src/utils/documetos.ts
@@ -42,3 +42,33 @@ export function formatarRG(rg: string) {
 
     return rgFormatado;
 }
+
+export function validarCPF(cpf: string){
+    cpf = cpf.replace(/[^\d]/g, '');
+
+    if (cpf.length !== 11) {
+        return false;
+    }
+    if (/^(\d)\1+$/.test(cpf)) {
+        return false;
+    }
+    let soma = 0;
+    for (let i = 0; i < 9; i++) {
+        soma += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+    let resto = 11 - (soma % 11);
+    let digitoVerificador1 = resto === 10 || resto === 11 ? 0 : resto;
+    if (digitoVerificador1 !== parseInt(cpf.charAt(9))) {
+        return false;
+    }
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+        soma += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    resto = 11 - (soma % 11);
+    let digitoVerificador2 = resto === 10 || resto === 11 ? 0 : resto;
+    if (digitoVerificador2 !== parseInt(cpf.charAt(10))) {
+        return false;
+    }
+    return true;
+}
